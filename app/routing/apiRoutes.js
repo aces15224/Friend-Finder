47c9f71e6c348d14265cb146e8c69978ad134af6
Anthony15224 <Anthony15224@hotmail.com>
2019-11-28 12:46:34 -0600

    routing complete

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,24 @@
+var friends = require("../data/friends");
+
+module.exports=function(app){
+app.get("/api/friends", function(req, res) {
+    return res.json(friends);
+  });
+
+
+app.post("/api/friends", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newFriend = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newFriend.name = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newFriend);
+
+  friends.push(newFriend);
+
+  res.json(newFriend);
+});
+}
