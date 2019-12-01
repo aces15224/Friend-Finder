020cdf2cf0c25d9775a67fbbdc376d37b1e40f9a
Anthony15224 <Anthony15224@hotmail.com>
2019-12-01 11:47:17 -0600

    make it better

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,25 @@
 var friends=[
 {
- name:"Joe-Bob",
- photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
- scores:[5,1,2,5,1,3,4,4,1,3]
+ name:"The Penguin",
+ photo:"https://www.cheatsheet.com/wp-content/uploads/2018/06/penguin-batman-animated-series.jpg",
+ scores:[1,5,5,4,5,3,5,2,1,5]
 },
 {
- name:"Jim-Bob",
- photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
- scores:[1,2,3,4,5,1,2,3,4,5]
+ name:"The Joker",
+ photo:"https://i.pinimg.com/originals/01/d9/c7/01d9c7f5c5ad0f82109e9a507196c0b9.jpg",
+ scores:[5,2,3,2,1,1,1,1,2,4]
 },
 {
- name:"Roberto",
- photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
- scores:[1,3,4,2,5,5,5,3,4,1]
-}
+ name:"Catwoman",
+ photo:"https://images.squarespace-cdn.com/content/v1/51b3dc8ee4b051b96ceb10de/1560884895415-Y3TB4UKEI3OF61ZX30M2/ke17ZwdGBToddI8pDm48kNvT88LknE-K9M4pGNO0Iqd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1USOFn4xF8vTWDNAUBm5ducQhX-V3oVjSmr829Rco4W2Uo49ZdOtO_QXox0_W7i2zEA/image-asset.jpeg?format=2500w",
+ scores:[2,5,3,3,4,3,5,5,5,3]
+},
 
+{
+ name:"Bane",
+ photo:"https://vignette.wikia.nocookie.net/batman/images/e/e5/Bane_animated.jpg/revision/latest?cb=20111206143913",
+ scores:[2,1,5,5,2,4,3,5,2,2]
+}
 ];
 console.log("yeehaw")
 module.exports=friends;
